9a28011467294beb0dc63a89043cff00c3f52f7e
Dominic Knabe <dominic.knabe@gmail.com>
2026-06-09 20:18:03 +1000

    mobile skills added backend section

--- resources/views/components/skills/skills.blade.php
+++ resources/views/components/skills/skills.blade.php
@@ -1,5 +1,7 @@
-<section class="flex flex-col items-center p-5">
+<section class="flex flex-col items-center p-5 gap-5">
     <h2 class="text-white text-4xl mb-10">SKILLS</h2>
-    <x-skills.cardComponent iconF="skills/html.png" iconS="skills/css.svg" iconT="skills/js.svg" iconFo="skills/alpine.png" iconFi="skills/livewire.png" iconSi="skills/tailwind.png" skill="Frontend" titleF="HTML5" titleS="CSS3" titleT="JavaScript"
-        titleFo="Alpine.js" titleFi="Livewire" titleSi="Tailwind"></x-skills.cardComponent>
+    <x-skills.cardComponent iconF="skills/html.png" iconS="skills/css.svg" iconT="skills/js.svg" iconFo="skills/alpine.png"
+        iconFi="skills/livewire.png" iconSi="skills/tailwind.png" skill="Frontend" titleF="HTML5" titleS="CSS3"
+        titleT="JavaScript" titleFo="Alpine.js" titleFi="Livewire" titleSi="Tailwind"></x-skills.cardComponent>
+    <x-skills.cardComponent skill="Backend"></x-skills.cardComponent>
 </section>
