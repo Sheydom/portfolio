20599942e60f4cfc33297081daf32c9aa0c130a7
Dominic Knabe <dominic.knabe@gmail.com>
2026-06-09 21:02:05 +1000

    desktop skills new section

--- resources/views/components/skills/skills.blade.php
+++ resources/views/components/skills/skills.blade.php
@@ -3,5 +3,6 @@
     <x-skills.cardComponent iconF="skills/html.png" iconS="skills/css.svg" iconT="skills/js.svg" iconFo="skills/alpine.png"
         iconFi="skills/livewire.png" iconSi="skills/tailwind.png" skill="Frontend" titleF="HTML5" titleS="CSS3"
         titleT="JavaScript" titleFo="Alpine.js" titleFi="Livewire" titleSi="Tailwind"></x-skills.cardComponent>
-    <x-skills.cardComponent skill="Backend"></x-skills.cardComponent>
+    <x-skills.cardComponent skill="Backend" titleF="PHP" titleS="Laravel" titleT="Eloquent ORM" titleFo="Authentication" titleFi="REST API" titleSi="Form Validation" iconF="skills/php.png" iconS="skills/laravel.ico"></x-skills.cardComponent>
+    <x-skills.cardComponent skill="Database"></x-skills.cardComponent>
 </section>
