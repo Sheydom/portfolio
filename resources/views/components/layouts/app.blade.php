dcbd914c84954b06c1e6f9a040d3100d186c0425
Dominic Knabe <dominic.knabe@gmail.com>
2026-06-21 21:47:20 +0800

    added meta tags and opengraph titles and images

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,6 +7,24 @@
 
     <title>{{ $title ?? config('app.name', 'Dominic Knabe Portfolio') }}</title>
 
+    <meta name="description"
+        content="Portfolio of Dominic Knabe, a Laravel and PHP developer specialising in self-hosted web applications, Docker deployments, Linux servers, and modern web development.">
+    >
+    <meta name="keywords" content="Laravel Developer, PHP Developer, Docker, Linux, MySQL, Web Developer, Melbourne">
+    <meta name="author" content="Dominic Knabe">
+
+    <meta property="og:title" content="Dominic Knabe | Laravel & PHP Developer">
+
+    <meta property="og:description"
+        content="Laravel, PHP, Docker, Linux, MySQL and self-hosted infrastructure projects.">
+
+    <meta property="og:image" content="https://dominic-knabe.com/images/preview.jpg">
+    <meta name="twitter:image" content="https://dominic-knabe.com/preview.jpg">
+
+    <meta property="og:url" content="https://dominic-knabe.com">
+
+    <meta property="og:type" content="website">
+
     <link rel="icon" href="/DK.jpg" sizes="any">
     <link rel="icon" href="/DK.jpg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/DK.jpg">
@@ -22,7 +40,7 @@
 </head>
 
 <body class="min-h-screen flex flex-col bg-background">
-     <x-layouts.navigation></x-layouts.navigation> 
+    <x-layouts.navigation></x-layouts.navigation>
     <x-layouts.main class="flex-1 flex flex-col ">{{ $slot }}</x-layouts.main>
     <div class="hidden sm:block" id="cursor-dot"></div>
     <div class="hidden sm:block" id="cursor-ring"></div>
